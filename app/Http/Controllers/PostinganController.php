fea84aa5cf8eb3f86f562f86a7949ff9eb8af090
William Tanardi <william.tanardi-2021@fst.unair.ac.id>
2023-06-13 13:18:43 +0700

    1

---
+++ app/Http/Controllers/PostinganController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostinganController extends Controller
+{
+   public function index()  {
+        $data = Post::all();
+        return view ('postingan/index', ['data' => $data]);
+    }
+
+    public function create(){
+        return view('postingan/create');
+    }
+
+    public function store(Request $request){
+        $data = [
+            'category_id' => $request-> category_id,
+            'title' => $request-> title,
+            'excerpt' => $request-> excerpt,
+            'body' => $request-> body,
+        ];
+        Post::create($data);
+        return view ('postingan/index');
+    }
+}
